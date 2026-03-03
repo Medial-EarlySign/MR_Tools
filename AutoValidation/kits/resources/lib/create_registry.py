0aa85531719b624fa3008fc5a329c1db3b5d4be6
Alon Lanyado <alonyado@gmail.com>
2026-03-03 16:27:39 +0200

    Rename columns in signal DataFrame for clarity

--- AutoValidation/kits/resources/lib/create_registry.py
+++ AutoValidation/kits/resources/lib/create_registry.py
@@ -51,7 +51,7 @@ if len(args.sub_codes) > 0:
         codes = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), fr.readlines())))
         fr.close()
         lut = rep.dict.prep_sets_lookup_table(rep.dict.section_id(signal), codes)
-        sig = rep.get_sig(signal, translate=False)
+        sig = rep.get_sig(signal, translate=False).rename(columns={'val':'val0', "date":"time0"}, errors='ignore')
         sig = sig[(lut[sig.val0] != 0)].sort_values('time0').drop_duplicates(subset=['pid'])[['pid', 'time0']].rename(columns={'time0':'group_time'})
         cases_g = cases[cases.pid.isin(sig.pid.tolist())].copy()
         cases_ng = cases[~cases.pid.isin(sig.pid.tolist())]
